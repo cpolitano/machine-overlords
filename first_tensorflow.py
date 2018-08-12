65b00e78fdfc5b85a77ab682e437a3f322c12a14
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-08-12 19:57:50 -0400

    import and describe data

--- first_tensorflow.py
+++ first_tensorflow.py
@@ -4,7 +4,7 @@ from matplotlib import cm
 from matplotlib import gridspec
 from matplotlib import pyplot as plt
 import numpy as np
-import pandas as import pd
+import pandas as pd
 #from sklearn import metrics
 import tensorflow as tf
 from tensorflow.python.data import Dataset
@@ -12,3 +12,15 @@ from tensorflow.python.data import Dataset
 tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
+
+# goal - predict median_house_value per input (such as total_rooms)
+# using Tensorflow Estimator API LinearRegressor interface
+
+california_housing_dataframe = pd.read_csv("https://dl.google.com/mlcc/mledu-datasets/california_housing_train.csv", sep=",")
+
+# reindex to enhance performance of stochastic gradient descent
+california_housing_dataframe = california_housing_dataframe.reindex(
+    np.random.permutation(california_housing_dataframe.index))
+california_housing_dataframe["median_house_value"] /= 1000.0
+print(california_housing_dataframe.head())
+print(california_housing_dataframe.describe())
