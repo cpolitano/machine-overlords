8250c90cfabcd752f6e8ba36175e3f4ac62d510a
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-09-08 13:25:54 -0400

    model layers and compile settings

--- fashion_vision.py
+++ fashion_vision.py
@@ -23,13 +23,25 @@ print(train_images.shape)
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+#
+# plt.show()
 
-plt.show()
+# defining the layers of the neural network
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)), # flatten 2d array of image pixels into 1 array
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# compile settings
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
