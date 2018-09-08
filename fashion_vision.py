6247a8e0af6740c55b48a7d41940a33ec23d02da
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-09-08 13:05:10 -0400

    setting up training data from fashion mnist

---
+++ fashion_vision.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+# plt.figure() # create new figure
+# plt.imshow(train_images[0]) # display image on axis
+# plt.colorbar() # add colorbar to plot
+# plt.grid(False) # turn grid off
+# plt.show()
+
+# scale values to float btw 0 and 1
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+
+plt.show()
