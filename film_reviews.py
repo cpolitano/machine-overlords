06b7aa23d4c08a8ca51cf3ce3f6b765d1743989e
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-09-16 15:30:14 -0400

    build model

--- film_reviews.py
+++ film_reviews.py
@@ -11,5 +11,54 @@ imdb = keras.datasets.imdb # review data preprocessed, words assigned as integer
 
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
-print(train_data[0])
-print(len(train_data[0]), len(train_data[1]))
+# print(train_data[0])
+# print(len(train_data[0]), len(train_data[1]))
+
+# Converting integers to words
+# A dictionary mapping words to an integer index
+word_index = imdb.get_word_index()
+
+# The first indices are reserved
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+# print(decode_review(train_data[1]))
+
+# use pad_sequences to make reviews the same length
+# inputs to a neural network must be the same length
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+
+# print(len(train_data[0]), len(train_data[1]))
+
+# Building model
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+print(model.summary())
+
+# using binary_crossentropy since this is a binary classification problem
+# and the model outputs of a probability (a single-unit layer with a sigmoid activation)
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
