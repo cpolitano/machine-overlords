ecbd73715a205633bf2c98cfa9c215c69631b885
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-09-16 15:54:28 -0400

    plot results

--- film_reviews.py
+++ film_reviews.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
@@ -62,3 +63,60 @@ print(model.summary())
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='binary_crossentropy',
               metrics=['accuracy'])
+
+# validation settingsx_val = train_data[:10000]
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+# train model
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+# evaluate results
+results = model.evaluate(test_data, test_labels)
+
+print(results) # returns loss and accuracy
+
+# model.fit returns history of training events
+# history_dict = history.history
+# history_dict.keys()
+# dict_keys(['val_acc', 'val_loss', 'acc', 'loss'])
+
+# plotting results
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+# plt.clf()   # clear figure
+# acc_values = history_dict['acc']
+# val_acc_values = history_dict['val_acc']
+#
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.xlabel('Epochs')
+# plt.ylabel('Accuracy')
+# plt.legend()
+#
+# plt.show()
