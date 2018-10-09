487d7b49fbd272a1502f7202885e2433f4f0faad
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-10-09 10:37:05 -0400

    normalize features

--- price_regression.py
+++ price_regression.py
@@ -13,6 +13,7 @@ boston_housing = keras.datasets.boston_housing
 # the features are aspects of the house or neighborhood
 # the label is house price in thousands USD
 (train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+print(train_data[0])
 print(train_labels[0:10])
 
 # Shuffle the training set
@@ -28,3 +29,11 @@ column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
 
 df = pd.DataFrame(train_data, columns=column_names)
 print(df.head())
+
+# Normalize features to compare features at different scales and ranges
+mean = train_data.mean(axis=0)
+std = train_data.std(axis=0)
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+
+print(train_data[0])  # First training sample, normalized
